d417d27f1f0b86218016dd63182a7ebcebe11ddc
Nate Tan <nktan93@gmail.com>
2019-03-12 14:50:45 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,7 @@
 require 'pry'
 
+animals = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"}
+
 class Hash
   def keys_of(*arguments)
     keys_array = []
